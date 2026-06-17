2482f4bfb1ccabd7f87fdf8fb4a819314e9f887d
Janice Escobar <janice.escobar@aol.com>
2026-06-17 10:42:26 -0400

    initial commit

---
+++ src/main/java/com/example/springdealership/SpringdealershipApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdealershipApplication.class, args);
+	}
+
+}
